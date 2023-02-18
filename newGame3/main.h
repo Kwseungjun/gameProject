7f8c9b7b8c226bdd6f3eec5eb040849361bebe88
seungjun <kwakseungjun6@gmail.com>
2023-02-18 19:35:47 +0900

    first game

---
+++ newGame3/main.h
@@ -0,0 +1,27 @@
+#ifndef __STDIO_H_
+
+#define __STDIO_H_
+#include <stdio.h>
+
+#endif
+
+#ifndef __STDLIB_H_
+
+#define __STDLIB_H_
+#include <stdlib.h>
+
+#endif
+
+#ifndef __CONIO_H_
+
+#define __CONIO_H_
+#include <conio.h>
+
+#endif
+
+#ifndef __WINDOWS_H_
+
+#define __WINDOWS_H_
+#include <windows.h>
+
+#endif
